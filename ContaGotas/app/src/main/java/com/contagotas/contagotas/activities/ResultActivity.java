6859d16c390a47aa293976b9116ffe419fabf56d
AFRSales <andrefernandes.sales@gmail.com>
2015-11-09 21:55:25 -0200

    Ajustes fluxo, tirado os settings das toolbars, ajuste layout da primeira tela indicando qual recinto pertence os dispositivos

--- ContaGotas/app/src/main/java/com/contagotas/contagotas/activities/ResultActivity.java
+++ ContaGotas/app/src/main/java/com/contagotas/contagotas/activities/ResultActivity.java
@@ -59,7 +59,6 @@ public class ResultActivity extends BaseActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_result, menu);
         return true;
     }
 
